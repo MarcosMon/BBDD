b44f1ea39bdd58a41477e04455fec9749f0a7bb8
marc monjon <sysmarc13@hotmail.com>
2019-03-26 00:57:28 +0100

    Segundo ejercicio

--- EjerciciosT3.sql
+++ EjerciciosT3.sql
@@ -25,5 +25,36 @@ DELIMITER ;
 SELECT NumerosDivisibles(12,4);
 
 SELECT NumerosDivisibles(15,4);
+
+/* Ejercicio 2: usa las estructuras condicionales para mostrar el día de la semana según un valor de entrada numérico, 1 para domingo, 
+2 lunes, etc. */
+DELIMITER //
+
+DROP FUNCTION IF EXISTS diasSemana;
+CREATE FUNCTION diasSemana(numeroDia int)
+		RETURNS varchar(10)
+	BEGIN
+        DECLARE diasSemana varchar(10);
+		IF numeroDia = 1 THEN
+            RETURN 'Lunes';
+		ELSEIF numeroDia = 2 THEN
+            RETURN 'Martes';
+		ELSEIF numeroDia = 3 THEN
+            RETURN 'Miercoles';
+		ELSEIF numeroDia = 4 THEN
+            RETURN 'Jueves';
+		ELSEIF numeroDia = 5 THEN
+            RETURN 'Viernes';
+		ELSEIF numeroDia = 6 THEN
+            RETURN 'Sabado';
+		ELSEIF numeroDia = 7 THEN
+            RETURN 'Domingo';
+		END IF;
+END; 
+DELIMITER ;
+
+SELECT diasSemana(5);
+
+SELECT diasSemana(3);
 			
 
